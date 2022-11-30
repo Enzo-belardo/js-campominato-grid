f2754ba38e5eaf795fdc864b4e2ba51998ab0305
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-11-30 17:13:29 +0100

    ulteriore implementazione

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-
-    
+ 
 for( let i = 1; i < 101; i++){
     let div = document.createElement('div');
     div.innerHTML = i;
+    div.classList.add('square')
     document.getElementById('container').appendChild(div);
 
 }
