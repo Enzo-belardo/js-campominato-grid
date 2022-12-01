bd92224a59431ea7df6e9102547b1fc9ca018e0e
Enzo-belardo <vincenzobelardo10@gmail.com>
2022-12-01 16:24:19 +0100

    implementato eventlist play

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 
  
-for( let i = 1; i < 101; i++){
-    let div = document.createElement('div');
-    div.innerHTML = i;
-    div.classList.add('square')
-    document.getElementById('container').appendChild(div);
+let pulsante= document.getElementById('pulsante')
 
-}
+pulsante.addEventListener('click', function(){
+
+for( let i = 1; i < 101; i++){
+  let div = document.createElement('div');
+  div.innerHTML = i;
+  div.classList.add('square')
+  div.addEventListener('click', function(){
+    div.classList.add('squareclick')
+  })
+  document.getElementById('container').appendChild(div);
+  }
+},{once: true})
+
+
